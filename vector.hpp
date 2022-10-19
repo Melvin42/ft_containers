d694b44e4148c1c402ddb206eca8ca687fd59d62
melperri <melperri@student.42.fr>
2022-10-19 02:44:01 +0200

    Stack bip boop

--- vector.hpp
+++ vector.hpp
@@ -22,7 +22,7 @@ namespace ft {
 				typedef typename allocator_type::const_reference			const_reference;
 				typedef typename allocator_type::pointer					pointer;
 				typedef typename allocator_type::const_pointer				const_pointer;
-				typedef ft::iterator<value_type>								iterator;
+				typedef ft::iterator<value_type>							iterator;
 				typedef ft::iterator<const value_type>						const_iterator;
 				typedef typename ft::reverse_iterator<iterator>				reverse_iterator;
 				typedef typename ft::reverse_iterator<const_iterator>		const_reverse_iterator;
